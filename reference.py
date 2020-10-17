433ecf4ea7b448d73a0395f4f8bddd47b78730d3
Zane <priebe3@outlook.com>
2020-10-17 13:56:02 -0700

    Added hashtag handling

--- reference.py
+++ reference.py
@@ -1,16 +1,28 @@
 # Hello this is Zane's file
 
 def main():
-    url = search('iPhone 12 Pro Max')
+    url = search('iPhone 12 Pro', '#Max #great')
     print(url)
 
 
-def search(query):
-    words = query.split(' ')
-    url = 'https://twitter.com/search?q='
-    for word in range(len(words) - 1):
-        url += words[word] + '%20'
-    url += words[len(words) - 1]
+def search(words, hashtags):
+    url = 'q='
+
+    words = words.split(' ')
+    if len(words) > 0:
+        url += '%20'.join(words)
+
+    hashtags = hashtags.split(' ')
+    if len(hashtags) > 0:
+        if(len(words) > 0):
+            url += '%20('
+        else:
+            url += '('
+        hash_query = []
+        for hashtag in hashtags:
+            hash_query.append(hashtag.replace('#', '%23'))
+        url += '%20OR%20'.join(hash_query)
+        url += ')'
 
     url += '&src=typed_query'
     return url

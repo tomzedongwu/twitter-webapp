1055654c7444bac72d84b63bfa19d4f3a100a9f6
Zane <priebe3@outlook.com>
2020-10-17 19:07:14 -0700

    Remade search

--- reference.py
+++ reference.py
@@ -4,49 +4,42 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
+
 def main():
     result = search('Donald Trump', '')
     card = make_cards(result)
     print(len(result))
 
+
 def make_card(status):
-    status=status.AsDict()
+    status = status.AsDict()
     full_text = if_has_key(status, "full_text")
     user = if_has_key(status, "user")
     hashtags = if_has_key(status, "hashtags")
-    retweeted_status = if_has_key(status, "retweeted_status") 
+    retweeted_status = if_has_key(status, "retweeted_status")
     return None
+
 
 def if_has_key(status, key):
     if key in status:
         return status[key]
     return None
 
+
 def make_cards(tweets):
     return [make_card(tweet) for tweet in tweets]
 
-def search(words, hashtags):
-    url = 'q='
 
-    words_str = ''
-    words = words.split(' ')
-    words_str = '%20'.join(words)
-
-    hashtags = hashtags.split(' ')
-    hash_query_str = ''
-    if hashtags[0] != '':
-        hash_query = [hashtag.replace('#', '%23') for hashtag in hashtags]
-        hash_query_str += '(' + '%20OR%20'.join(hash_query) + ')'
-    
-    url += words_str
-    if(len(hash_query_str) > 0):
-        url += '%20' + hash_query_str
-    url += "%20lang%3Aen&count=100"
-
-    print(url)
-
+def newSearch(words, hashtags):
     api = authenticateApi()
-    results = api.GetSearch(raw_query=url)
+    query = words + hashtags
+    results = api.GetSearch(term=query, count=100, lang='en',
+                            result_type='mixed')
+    print(results[0].created_at)
+    print(len(results))
+    for i in range(len(results)):
+        print(i + 1, results[i].full_text)
+        print()
     return results
 
 
@@ -77,6 +70,3 @@ def authenticateApi():
 
 if __name__ == '__main__':
     main()
-
-
-

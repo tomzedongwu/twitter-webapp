463202ff93f3e348640b6d2f0250f8ad2b183297
assafv <assafv@localhost.localdomain>
2020-10-18 05:13:05 -0700

    adding to .gitignore

--- reference.py
+++ reference.py
@@ -5,7 +5,7 @@ import dash
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-import re as re;
+import re
 
 def get_tweet_cards(terms, hashtags, accounts):
     result = search(terms, hashtags, accounts)
